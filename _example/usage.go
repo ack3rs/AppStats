e264c443a80ee2894ddd295376112ce85564e78f
Mark Ackroyd <mark@ackroyd.net>
2021-07-03 12:32:05 +0100

    WIP: Started to Add an example

--- _example/usage.go
+++ _example/usage.go
@@ -1,8 +1,27 @@
 package main
 
+import (
+	"os"
+	"time"
+
+	"github.com/acky666/AppStats"
+)
+
 func main() {
 
-	// TODO: 
+	Hostname, _ := os.Hostname()
 
+	AppStats.Go(AppStats.Config{Version: "v0.0.1",
+		Hostname:        Hostname,
+		Environment:     "development",
+		EnvironmentType: "development",
+		InfluxURL:       "http://influx.example.com/:8086/write?db=AppStats",
+		Project:         "Example",
+		UpdateFrequency: 15 * time.Second,
+	})
+
+	// TODO:   Web Handler.
+
+	// Web Server
 
 }
